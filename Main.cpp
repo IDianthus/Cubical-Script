523c78786504865f0910f2d10f713b9b7ab4bbcb
Codrineye <dumitrescualexandrucodrin@gmail.com>
2024-02-26 14:25:02 +0200

    Finish Monday

--- Main.cpp
+++ Main.cpp
@@ -7,38 +7,38 @@ ifstream in("Test.in");
 ofstream out("Test.out");
 
 int clear;
-char word [3];
+char word[3];
 
-int main()
-{
-	while ( in >> word [ 2 ] )
+int main() {
+	for( int i = 65; char(i) != 'Z'; i ++ )
 	{
-		if( word [ 2 ] == '{' || word [ 2 ] == '(' )
-		{
+		cout << i << endl;
+	}
+	return 0;
+	while (in >> word[2]) {
+		if (word[2] == '{' || word[2] == '(') {
 			clear ++;
 		}
-		if( word [ 2 ] == '}' || word [ 2 ] == ')' )
-		{
+		if (word[2] == '}' || word[2] == ')') {
 			clear --;
 		}
-		if ( word [ 1 ] == char(92) && word [ 2 ] == 'n'  )
-		{
-			out << " " << endl;
-			for( int i = clear; i > 0; i -- )
-			{
+		if (word[1] == char(92) && word[2] == 'n') {
+			if (word[0] != ';') {
+				out << " ";
+			}
+			out << endl;
+			for(int i = clear; i > 0; i --) {
 				out << char(9);
 			}
 		}
-		else if( word [ 1 ] == char(92) && word [ 2 ] == 's' )
-		{
+		else if (word[1] == char(92) && word[2] == 's') {
 			out << " ";
 		}
-		else if ( (word [ 2 ] != char( 92 ) ) || ( word[ 1 ] == char(92) && word[ 2 ] == char( 92 ) ) )
-		{
-			out << word [ 2 ] ;
+		else if (word[2] != char(92) || word[1] == char(92)) {
+			out << word[2];
 		}
-		word [ 0 ] = word [ 1 ];
-		word [ 1 ] = word [ 2 ];
+		word[0] = word[1];
+		word[1] = word[2];
 	}
 	return 0;
 }
