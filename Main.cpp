d9cab6d0b4c51698fa77c7a48ce47c9351e922a1
Codrineye <dumitrescualexandrucodrin@gmail.com>
2024-03-02 15:45:46 +0200

    I want to be syncronised

--- Main.cpp
+++ Main.cpp
@@ -17,19 +17,36 @@ int main() {
 		if (word[2] == '}' || word[2] == ')') {
 			clear --;
 		}
-		if (word[1] == char(92) && word[2] == 'n') {
-			if (word[0] != ';') {
+		if (word[1] == char(92)) {
+			if( word[2] == 'e') {
+				out << '\n';
+				for (int i = clear - 1; i > 0; i --) {
+					out << '\t';
+				}
+			}
+			else if(word[2] == 'n') {
+				out << '\n';
+				for (int i = clear; i > 0; i --) {
+					out << '\t';
+				}
+			}
+			else if(word[2] == 's') {
 				out << " ";
 			}
-			out << endl;
-			for(int i = clear; i > 0; i --) {
-				out << char(9);
+			else if(word[2] == 't') {
+				out << '\t';
+			}
+			else if (word[2] == char(92)) {
+				if (word[0] != '(' && word[0] != ')') {
+					out << " ";
+				}
+				out << char(92);
+			}
+			else {
+				out << word[2];
 			}
 		}
-		else if (word[1] == char(92) && word[2] == 's') {
-			out << " ";
-		}
-		else if (word[2] != char(92) || word[1] == char(92)) {
+		else if (word[2] != char(92)) {
 			out << word[2];
 		}
 		word[0] = word[1];
