a25ff7a33b1073c25a05ee753e3dcaa356b93162
Codrineye <dumitrescualexandrucodrin@gmail.com>
2024-03-08 18:22:03 +0200

    Refactored

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 ifstream in("Test.in");
 ofstream out("Test.out");
 
+
+
 void indenting(int clear) {
 	out << endl;
 	for (int i = clear; i > 0; i--) {
@@ -14,65 +16,66 @@ void indenting(int clear) {
 }
 
 bool comment;
-int clear;
-char word [3];
-
+int indentation;
+char input [3];
 
 int main() {
-	while (in.get(word [2])) {
-		if (word [2] == '{' || word [2] == '(') {
+	while (in.get(input [2])) {
+		if (input [2] == '{' || input [2] == '(') {
 			if (comment == false) {
-				clear++;
+				indentation++;
 			}
 		}
-		if (word [2] == '}' || word [2] == ')') {
+		if (input [2] == '}' || input [2] == ')') {
 			if (comment == false) {
-				clear--;
+				indentation--;
 			}
 		}
-		if (int(word [0]) > 47 && int(word [0]) < 58 && word [1] == '.' && word [2] == ')') {
+		if (int(input [0]) > 47 && int(input [0]) < 58 && input [1] == '.' && (int(input [2]) < 48) || int(input [2] > 57) ) {
 			out << 0;
 		}
-		if (word [1] == ':' && word [2] == '{' || word [1] == '}' && word [2] == ';') {
-			out << word [2];
-			indenting(clear);
+		if (input [1] == ':' && input [2] == '{' || input [1] == '}' && input [2] == ';') {
+			out << input [2];
+			indenting(indentation);
 		}
-		else if (word [1] == '{' && word [2] == '"') {
-			indenting(clear);
-			out << word [2];
+		else if (input [1] == '{' && input [2] == '"') {
+			indenting(indentation);
+			out << input [2];
 		}
-		else if (word [1] == '\\') {
-			if (word [2] == 'c') {
+		else if (input [1] == '\\') {
+			if (input [2] == 'c') {
 				comment = true;
 			}
-			if (word [2] == '}') {
-				indenting(clear);
-				out << word [2];
+			if (input [2] == '}') {
+				indenting(indentation);
+				out << input [2];
 			}
-			else if (word [2] == 'n') {
-				comment = false;
-				indenting(clear);
+			else if (input [2] == 'n') {
+				if(input [0] =! '\\') {
+					
+				}
+				indenting(indentation);
 			}
-			else if (word [2] == '\\') {
-				if (word [0] != '(' && word [0] != ')') {
+			else if (input [2] == '\\') {
+				if (input [0] != '(' && input [0] != ')') {
 					out << " ";
 				}
 				out << '\\';
 			}
 			else {
-				out << word [2];
+				out << input [2];
 			}
 		}
-		else if (word [2] != '\\') {
-			out << word [2];
+		else if (input [2] != '\\') {
+			out << input [2];
 		}
-		if (word [2] == word [1] && word [1] == word [0] && word [0] == ' ') {
+		if (input [2] == input [1] && input [1] == input [0] && input [0] == ' ') {
 			out << '\b' << '\b' << '\b' << '\t';
-			word [2] = word [1];
-			word [1] = word [0];
+			input [2] = input [1];
+			input [1] = input [0];
 		}
-		word [0] = word [1];
-		word [1] = word [2];
+		input [0] = input [1];
+		input [1] = input [2];
 	}
 	return 0;
 }
--- scr/Lexer.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	char test[5][5];
-	cout << test[0][7];
-	cin >> test[0][0];
-	return 0;
-}
